a29d910706f57d4e9accb72908ea7d44ad81fb08
Vinay Samuel <33227942+vsamuel2003@users.noreply.github.com>
2024-05-29 09:08:22 -0500

    Add files via upload

---
+++ code/LLaVA/setup_llava.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Clone the repository
+git clone https://github.com/haotian-liu/LLaVA.git
+
+# Navigate into the repository directory
+cd LLaVA
+
+# Install the package in editable mode
+pip install -e .
